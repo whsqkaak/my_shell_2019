30afac6caa04a5ab13098ab9df61c7835b67aac4
LSH <whsqkaak@naver.com>
2019-04-26 05:30:24 +0900

    cat

--- my_shell.c
+++ my_shell.c
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 #define CMD_LEN 100 // The Maximum command length.
 
 
@@ -15,7 +18,9 @@ int main(void){
     char *args[CMD_LEN + 1]; // command line arguments.
     char *input; // Input command.
     int i;
-    // Get input command repeatedly
+    int status;
+   
+   // Get input command repeatedly 
     while (1) {
         printf("LSH_SHELL>>> ");
 	fflush(stdout); // Flush stdout buffer.
@@ -24,7 +29,7 @@ int main(void){
         fgets(input, CMD_LEN, stdin);
 
         i = 0;
-	args[i] = strtok(input, " \n");
+	args[0] = strtok(input, " \n");
 
         // strtok input command line
 	while(args[i] != NULL){
@@ -40,6 +45,36 @@ int main(void){
         // exit command
 	if(strcmp(args[0], "exit")==0){
             break;
+	}
+
+	// fork  아무 의미없는 명령어 입력 시에도 프로세스가 fork되야하나
+	pid_t pid = fork();
+	// fork error
+	if (pid < 0){
+	    perror("Fork error");
+	    exit(0);
+	}
+
+	// child process
+	if (pid == 0){
+	    execv(args[0], args);
+	}
+	// parent process
+	if (pid > 0){
+	    int retval = waitpid(pid, &status, 0);
+	    if (retval > 0){
+	        if(WIFEXITED(status)){
+		    printf("Child exited by process completion : %d\n", WEXITSTATUS(status));
+		}
+		if(WIFSIGNALED(status)){
+		    printf("Child exited by signal : %d\n", WTERMSIG(status));
+		}
+	    }
+	    else{
+	        printf("Unexpected error.\n");
+		exit(0);
+	    }
+	    printf("Child process complete.\n");
 	}
 
         no_input:;
